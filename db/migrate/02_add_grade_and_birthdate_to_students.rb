212409ad3c4157e1bde31c3744f826d7920b89fd
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-11 20:04:24 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,9 +1,9 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
 
   def change
-    create_table :students do |t|
-      t.string :name
-    end 
-  end 
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
+  end
+end
   
 end 
